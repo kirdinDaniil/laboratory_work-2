8eb5f4936fb7135fe0846afa6d74e777b96089c0
Kirdin Daniil <Zuzorqa@gmail.com>
2022-10-20 21:43:08 +0300

    123

--- OP/OP_Lab4/cmake-build-debug/cmake_install.cmake
+++ OP/OP_Lab4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zuzorqa/Desktop/первая неклассическая папка/основы/лабы/untitled3
+# Install script for directory: /Users/zuzorqa/Desktop/laboratory_work-2/OP/OP_Lab4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zuzorqa/Desktop/первая неклассическая папка/основы/лабы/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zuzorqa/Desktop/laboratory_work-2/OP/OP_Lab4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- OP/OP_Lab5/cmake-build-debug/cmake_install.cmake
+++ OP/OP_Lab5/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zuzorqa/Desktop/первая неклассическая папка/основы/лабы/untitled4
+# Install script for directory: /Users/zuzorqa/Desktop/laboratory_work-2/OP/OP_Lab5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zuzorqa/Desktop/первая неклассическая папка/основы/лабы/untitled4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zuzorqa/Desktop/laboratory_work-2/OP/OP_Lab5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
